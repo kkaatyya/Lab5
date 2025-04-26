f2a47eedb635ef6c91bf095a7f71bf24f891c644
kkaatyya <151466280+kkaatyya@users.noreply.github.com>
2025-04-26 21:33:43 +0300

    Add files via upload
    додано поток під кожного клієнта

--- main.cpp
+++ main.cpp
@@ -4,70 +4,83 @@
 #include <string>
 #include <winsock2.h>
 #include <ws2tcpip.h>
+#include <thread>
+
+using namespace std;
 
 #define PORT 8080
-#define HTML_CONTENT "htmlfiles"
-void sendResponse(SOCKET clientSocket, const std::string& response)
+#define ROOT_DIR "htmlfiles"
+void sendResponse(SOCKET clientSocket, const string& response)
 {
     send(clientSocket, response.c_str(), response.size(), 0);
 }
+
 void handleRequest(SOCKET clientSocket)
 {
-    std::string httpResponse = "HTTP/1.1 200 OK\r\nContent-Length: " +
-    std::to_string(strlen(HTML_CONTENT)) + "\r\n\r\n" + HTML_CONTENT;
+    string httpResponse = "HTTP/1.1 200 OK\r\nContent-Length: " +
+        to_string(strlen(ROOT_DIR)) + "\r\n\r\n" + ROOT_DIR;
     sendResponse(clientSocket, httpResponse);
     closesocket(clientSocket);
 }
+
 int main()
 {
     WSADATA wsaData;
     WSAStartup(MAKEWORD(2, 2), &wsaData);
+
     SOCKET serverSocket;
-    struct sockaddr_in serverAddr;
+    sockaddr_in serverAddr;
+
     serverSocket = socket(AF_INET, SOCK_STREAM, 0);
     if (serverSocket == INVALID_SOCKET)
     {
-        std::cerr << "Socket creation failed\n";
+        cerr << "Socket creation failed\n";
         WSACleanup();
         return 1;
     }
+
     memset(&serverAddr, 0, sizeof(serverAddr));
     serverAddr.sin_family = AF_INET;
-
     serverAddr.sin_addr.s_addr = htonl(INADDR_ANY);
     serverAddr.sin_port = htons(PORT);
-    if (bind(serverSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) ==SOCKET_ERROR)
+
+    if (bind(serverSocket, (sockaddr*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR)
     {
-        std::cerr << "Bind failed\n";
+        cerr << "Bind failed\n";
         closesocket(serverSocket);
         WSACleanup();
         return 1;
     }
+
     if (listen(serverSocket, 5) == SOCKET_ERROR)
     {
-        std::cerr << "Listen failed\n";
+        cerr << "Listen failed\n";
         closesocket(serverSocket);
         WSACleanup();
         return 1;
     }
-    std::cout << "Server listening on port " << PORT << "...\n";
+
+    cout << "Server listening on port " << PORT << "...\n";
+
     while (true)
     {
-        struct sockaddr_in clientAddr;
+        sockaddr_in clientAddr;
         int clientAddrLen = sizeof(clientAddr);
-        SOCKET clientSocket = accept(serverSocket, (struct sockaddr*)&clientAddr,
-        &clientAddrLen);
+        SOCKET clientSocket = accept(serverSocket, (sockaddr*)&clientAddr, &clientAddrLen);
         if (clientSocket == INVALID_SOCKET)
         {
-            std::cerr << "Accept failed\n";
+            cerr << "Accept failed\n";
             continue;
         }
+
         char client_addr[INET_ADDRSTRLEN];
         inet_ntop(AF_INET, &(clientAddr.sin_addr), client_addr, INET_ADDRSTRLEN);
-        std::cout << "Connection accepted from " << client_addr << ":" <<
-        ntohs(clientAddr.sin_port) << std::endl;
-        handleRequest(clientSocket);
+        cout << "Connection accepted from " << client_addr << ":" << ntohs(clientAddr.sin_port) << endl;
+
+        thread clientThread(handleRequest, clientSocket);
+        clientThread.detach();
     }
+
     closesocket(serverSocket);
     WSACleanup();
     return 0;
